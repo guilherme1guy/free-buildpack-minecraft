4d65eba431adf5ecbb660d3130705f843b04cf9a
Guilherme Guy <guilhermeguy349@gmail.com>
2018-09-08 13:35:01 +0000

    Adds minecraft version display

--- opt/index.js
+++ opt/index.js
@@ -10,18 +10,19 @@ var regex = /URL:tcp:\/\/0.tcp.ngrok.io:(\d{5})/g
 var port = regex.exec(log)[1]
 
 // Check if there is a discord webhook setup
-var webhook_url = process.env.DISCORD_WEBHOOK_URL
+const webhook_url = process.env.DISCORD_WEBHOOK_URL
+const minectaft_version = process.env.MINECRAFT_VERSION
 
 if (webhook_url != null){
   // Call the webhook with the server address
 
   const Hook = new Webhook(webhook_url)
-  Hook.info('Minecraft Server','Server Address: 0.tcp.ngrok.io:' + port + "         -----   If you can't connect, wait a few minutes")
+  Hook.info('Minecraft Server','Server Address: 0.tcp.ngrok.io:' + port + "      -------  Minecraft version: " + minectaft_version + "         -----   If you can't connect, wait a few minutes")
 
 
 }
 
 http.createServer(function (request, response) {
   response.writeHead(200, { 'Content-Type': 'application/json' })
-  response.end('Server Address: 0.tcp.ngrok.io:' + port)
+  response.end('Version: '+ minectaft_version +'Server Address: 0.tcp.ngrok.io:' + port)
 }).listen(process.env.PORT || 8080)
